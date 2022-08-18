71666c18c0cb9148ba2a7ee255f852ae85cd7f28
Nishant Shaklan <90901154+shaklan2001@users.noreply.github.com>
2022-08-18 18:00:01 +0530

    Time: 55 ms (5.26%), Space: 17.1 MB (5.03%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -1,26 +1,100 @@
+class TrieNode{
+    public:
+        char data;
+        TrieNode* children[26];
+        int childCount;
+        bool isTerminal;
+
+        TrieNode(char ch){
+            data = ch;
+            for(int i=0; i<26; i++){
+                children[i] = NULL;
+            }
+            childCount = 0;
+            isTerminal = false;
+        }
+
+};
+
+class Trie{
+    public:
+    TrieNode* root;
+
+    Trie(char ch) {
+        root = new TrieNode(ch);
+    }
+    
+    void insertUtil(TrieNode* root, string word){
+        //base case
+        if(word.length() == 0){
+            root -> isTerminal = true;
+            return ;
+        }
+
+        int index = word[0] - 'a';
+        TrieNode* child;
+
+        //present
+        if( root -> children[index] != NULL){
+            child = root -> children[index];
+        }
+        else{
+            //absent
+            child = new TrieNode(word[0]);
+            root -> childCount++;
+            root -> children[index] = child;
+        }
+
+        //recursion
+        insertUtil(child, word.substr(1));
+    }
+
+    void insertWords(string word){
+        insertUtil(root, word);
+    }
+    
+    void lcp(string str, string &ans){
+        for(int i=0; i<str.length(); i++){
+            char ch = str[i];
+            if(root -> childCount == 1){
+                ans.push_back(ch);
+                int index = ch - 'a';
+                root = root->children[index];
+            }
+            else{
+                break;
+            }
+            
+            
+            if(root -> isTerminal)
+                break;
+        }
+    }
+};
+
+
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
+        Trie *t = new Trie('\0');
         int n = strs.size();
-        string ans = "";
-        for(int i=0; i<strs[0].length(); i++){
-            char ch = strs[0][i];
-            
-            bool match = true;
-            
-            //for comparing ch with rest os the string
-            for(int j=1; j<n; j++){
-                //not match
-                if(strs[j].size() < i || ch != strs[j][i]){
-                    match = false;
-                    break;
-                }
-            }
-            if(match == false)
-                break;
-            else
-                ans.push_back(ch);
+        //insert kardo all strings into trie
+        for(int i=0; i<n; i++){
+            t->insertWords(strs[i]);
         }
+        
+        for(int i=0; i<n; i++){
+            if(strs[i] == ""){
+                return "";
+            }
+        }
+        
+        
+        
+        string first = strs[0];
+        string ans = "";
+        
+        t->lcp(first, ans);
         return ans;
     }
 };
